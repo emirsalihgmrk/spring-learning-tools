1b81a3fdeef5b32e8f6a005eefe27bfc036e2d3c
emirsalihgmrk <emirsalihtrbzn@gmail.com>
2025-07-03 01:10:26 +0300

    .

---
+++ spring-security/section4/src/main/java/com/example/section3/EasyBankBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.section3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// @ComponentScan("com.example.section1.controller")
+public class EasyBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBankBackendApplication.class, args);
+	}
+
+}
