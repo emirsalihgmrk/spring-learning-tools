9e6d7f619983e4d5f5e0fcb8d8777589b2977012
emirsalihgmrk <emirsalihtrbzn@gmail.com>
2025-07-04 23:33:41 +0300

    .

---
+++ spring-security/section6/src/test/java/com/example/section6/EasyBankBackendApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.section6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBankBackendApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
