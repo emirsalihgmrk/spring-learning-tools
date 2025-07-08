dbb4b28ab16208b4dba248ea162f7f78eb9c0e27
emirsalihgmrk <emirsalihtrbzn@gmail.com>
2025-07-09 00:47:31 +0300

    .

--- spring-security/section9/src/main/java/com/example/section8/EasyBankBackendApplication.java
+++ spring-security/section9/src/main/java/com/example/section8/EasyBankBackendApplication.java
@@ -2,9 +2,10 @@ package com.example.section8;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-// @ComponentScan("com.example.section1.controller")
+@EnableWebSecurity(debug = true)
 public class EasyBankBackendApplication {
 
 	public static void main(String[] args) {
