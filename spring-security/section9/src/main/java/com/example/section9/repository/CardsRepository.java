f89893fe0c0a0c1d98cc8d2358f7eb1a18bc43dc
emirsalihgmrk <emirsalihtrbzn@gmail.com>
2025-07-09 01:02:11 +0300

    package name is changed

---
+++ spring-security/section9/src/main/java/com/example/section9/repository/CardsRepository.java
@@ -0,0 +1,15 @@
+package com.example.section9.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.section9.model.Cards;
+
+@Repository
+public interface CardsRepository extends CrudRepository<Cards, Long> {
+	
+	List<Cards> findByCustomerId(long customerId);
+
+}
